f87386c416dba579227c1a5d82f8b84d11683232
Ditray <76690157+Ditray@users.noreply.github.com>
2021-02-14 20:25:59 +0300

    Add files via upload

---
+++ Social Network/Social Network/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Social Network
+//
+//  Created by Андрей Дитякин on 14.02.2021.
+//
+		
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBOutlet var loginField: UITextField!
+    @IBOutlet var passwordDield: UITextField!
+
+    @IBAction func button(_ sender: UIButton) {
+    }
+}
