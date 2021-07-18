abab2de1f6a38b9640652eab2287648a3b57a3a4
Ditray <hondadit@mail.ru>
2021-07-18 18:50:32 +0300

    First lesson
    Added view for authorization

---
+++ SocialNetwork/SocialNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by Андрей Дитякин on 17.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
