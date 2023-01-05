11fd407bc3bf19a2aa8ce39f8bc28598d835a59f
Ditray <hondadit@mail.ru>
2023-01-05 21:33:32 +0300

    AuthVk added

--- FirstProject/FirstProject/LoginViewController.swift
+++ FirstProject/FirstProject/LoginViewController.swift
@@ -6,74 +6,75 @@
 //
 
 import UIKit
+import WebKit
 
-class LoginViewController: UIViewController {
-    let login:String = ""
-    let pass:String = ""
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var loginTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBAction func loginButton(_ sender: UIButton) {
-        if login == loginTextField.text {
-            loginTextField.backgroundColor = UIColor.green
-        }
-        else {
-            loginTextField.backgroundColor = UIColor.gray
-        }
-        if pass == passwordTextField.text {
-            passwordTextField.backgroundColor = UIColor.green
-        }
-        else {
-            passwordTextField.backgroundColor = UIColor.gray
-        }
-    }
-    
-    
-    
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        let Check = CheckUserData()
-        if !Check
-        { showLoginError()}
-        return Check
-    }
-    func CheckUserData() -> Bool {
-        if login == loginTextField.text && pass == passwordTextField.text
-        {return true} else
-        {return false}
-    }
-    func showLoginError() { // Создаем контроллер
-    let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
-    // Создаем кнопку для UIAlertController
-    let action = UIAlertAction(title: "OK", style: .cancel, handler: nil) // Добавляем кнопку на UIAlertController
-    alter.addAction(action)
-    // Показываем UIAlertController
-    present(alter, animated: true, completion: nil) }
+final class LoginViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureView()
+        loadAuth()
     }
+}
+extension LoginViewController: WKNavigationDelegate {
     
-    // Когда клавиатура появляется
-    @objc func keyboardWasShown(notification: Notification) {
-            // Получаем размер клавиатуры
-    let info = notification.userInfo! as NSDictionary
-    let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
-    let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
-            // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
-    self.scrollView?.contentInset = contentInsets
-    scrollView?.scrollIndicatorInsets = contentInsets }
-        //Когда клавиатура исчезает
-    @objc func keyboardWillBeHidden(notification: Notification) {
-        let contentInsets = UIEdgeInsets.zero
-        scrollView?.contentInset = contentInsets
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        
+        guard
+            let url = navigationResponse.response.url,
+            url.path == "/blank.html",
+            let fragment = url.fragment() else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        let params = fragment
+            .components(separatedBy: "&")
+            .map {$0.components(separatedBy: "=")}
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+            }
+        if let token = params["access_token"], let id = params["user_id"] {
+            Session.shared.userId = Int(id)!
+            Session.shared.token = token
+            decisionHandler(.cancel)
+            let vc = (storyboard?.instantiateViewController(withIdentifier: "TabBar"))!
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
-    override func viewWillAppear(_ animated: Bool) { super.viewWillAppear(animated)
-            // Подписываемся на два уведомления: одно приходит при появлении клавиатуры
-    NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
-            // Второе — когда она пропадает
-    NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
+}
+ private extension LoginViewController {
+    
+     func configureView() {
+         
+         webView.navigationDelegate = self
+     }
+     
+     func loadAuth() {
+         
+         var urlComponents = URLComponents()
+                 urlComponents.scheme = "https"
+                 urlComponents.host = "oauth.vk.com"
+                 urlComponents.path = "/authorize"
+                 urlComponents.queryItems = [
+                     URLQueryItem(name: "client_id", value: "51508114"),
+                     URLQueryItem(name: "display", value: "mobile"),
+                     URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+                     URLQueryItem(name: "scope", value: "offline, friends, photos, groups"),
+                     URLQueryItem(name: "response_type", value: "token"),
+                     URLQueryItem(name: "revoke", value: "0")
+                 ]
+                 
+                 let request = URLRequest(url: urlComponents.url!)
+                 webView.load(request)
 
+
+     }
 }
 
